ed4d61be0f451bcf6de3a7038503c1ee538d8df8
Jordan Roberts <jordanroberts@talk21.com>
2019-09-09 16:06:11 +0100

    Click count now updates also

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   // Declare a new state variable, which we will call "count":
-  const [count, setCount] = useState(0); 
+  const [count, setCount] = useState(0);
   const [person, setPerson] = useState([]);
 
-  // Similar to componentDidMount  and componentDidUpdate: 
+  // Similar to componentDidMount  and componentDidUpdate:
   useEffect(() => {
     getPerson();
-  },[]);
+  }, []);
 
   const getPerson = async () => {
-    const response = await fetch(
-      "https://api.randomuser.me");
-      const data = await response.json();
-    
-      console.log(data.results);
-      setPerson(data.results);
+    const response = await fetch("https://api.randomuser.me");
+    const data = await response.json();
+    setPerson(data.results);
   };
-  
+
   return (
     <div className="App">
       <p>You clicked {count} times</p>
-      <button onClick={() => getPerson()}>Click Me</button>
+      <button onClick={() => {getPerson(); setCount(count + 1)}}>Click Me</button>
       <div>{person.map(person => person.name.first)}</div>
     </div>
   );
 }
-// person&& = it will only render if there's a person
 
 export default App;
