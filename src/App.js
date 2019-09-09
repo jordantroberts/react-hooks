113436372d2f59bfbb668f8d46427f9ee04525a4
Jordan Roberts <jordanroberts@talk21.com>
2019-09-09 11:27:29 +0100

    Added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we will call "count":
   const [count, setCount] = useState(0); 
+
+  // Similar to componentDidMount  and componentDidUpdate: 
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div className="App">
